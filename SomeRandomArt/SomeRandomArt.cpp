21abeec5da415f39ee21a6ae69d5a5305a992d04
James-Holloway <james.holloway@pyrites.org.uk>
2023-09-07 05:18:04 +0100

    Multithreaded Fractal Generation Reduced number of duplicated code by merging the output stage into a FractalFunction func

--- SomeRandomArt/SomeRandomArt.cpp
+++ SomeRandomArt/SomeRandomArt.cpp
@@ -12,6 +12,8 @@ const uint32_t size = 1024;
 const uint32_t width = size;
 const uint32_t height = size;
 
+const uint32_t threadCount = 16;
+
 void GenerateUV(cv::Mat& image)
 {
     uint i = 0, x = 0, y = 0;
@@ -35,167 +37,116 @@ void GenerateUV(cv::Mat& image)
 }
 
 
-void GenerateMandlebrotSet(cv::Mat& image)
+using FractalFunction = std::function<void(uint32_t x, uint32_t y, uint32_t iterations, uint32_t maxIterations, uchar value, cv::Mat* image)>;
+
+uchar MandelbrotIteration(float u, float v, uint32_t maxIterations, uint32_t* iterations)
 {
-    const uint32_t maxIteration = 32;
-
-    uint i = 0, x = 0, y = 0;
-    float u = 0, v = 0;
-
-    uint32_t iteration = 0;
-    float fraction;
-    uint32_t value;
-
-    printf("Generating Mandlebrot Set with %i max iterations\r\n", maxIteration);
-
-    for (i = 0; i < size * size; i++)
+    std::complex<float> z, c = { 2.0f * u - 1.5f, 2.0f * v - 1 };
+    uint32_t i = 0;
+    for (i = 0; i < maxIterations; i++)
     {
-        x = i % size;
-        y = floor(i / size);
-
-        u = x / (float)size;
-        v = y / (float)size;
-
-        std::complex<float> z, c = { 2.0f * x / width - 1.5f, 2.0f * y / height - 1 };
-
-        for (iteration = 0; iteration < maxIteration; iteration++)
+        if (abs(z) >= 2)
         {
-            if (abs(z) >= 2)
-            {
-                break;
-            }
-            z = z * z + c;
+            break;
         }
+        z = z * z + c;
+    }
 
-        fraction = iteration / (float)maxIteration;
-        value = fraction * 255;
+    if (iterations != nullptr)
+    {
+        *iterations = i;
+    }
 
-        // Red
-        image.at<cv::Vec3b>(y, x)[2] = value;
-        // Green
-        image.at<cv::Vec3b>(y, x)[1] = value;
-        // Blue
-        image.at<cv::Vec3b>(y, x)[0] = value;
+    return (i / (float)maxIterations) * 255;
+}
+
+void MandelbrotSet(cv::Mat* image, FractalFunction func, uint32_t maxIterations, uint32_t offset, uint32_t batchSize)
+{
+    for (uint i = offset; i < offset + batchSize; i++)
+    {
+        uint32_t x = 0, y = 0;
+        float u = 0, v = 0;
+        x = i % width;
+        y = floor(i / height);
+
+        u = x / (float)width;
+        v = y / (float)height;
+
+        if (func != nullptr)
+        {
+            uint32_t iterations = 0;
+            uchar value = MandelbrotIteration(u, v, maxIterations, &iterations);
+            func(x, y, iterations, maxIterations, value, image);
+        }
+    }
+}
+
+void GenerateMandlebrotSet(cv::Mat* image, FractalFunction func, const uint32_t maxIterations = 32)
+{
+    if (func == nullptr)
+    {
+        throw std::invalid_argument("No func provided");
+    }
+
+    printf("Generating Mandlebrot Set with %i max iterations over %i threads\r\n", maxIterations, threadCount);
+
+    uint32_t batchSize = (width * height) / threadCount;
+
+    std::vector<std::thread> threads{};
+    for (uint t = 0; t < threadCount; t++)
+    {
+        uint32_t offset = batchSize * t;
+        std::thread thread([=]()
+            {
+                printf("Mandelbrot thread %i dispatched\r\n", t);
+                MandelbrotSet(image, func, maxIterations, offset, batchSize);
+            }
+        );
+        threads.push_back(std::move(thread));
+    }
+
+    printf("All Mandlebrot threads dispatched\r\n");
+
+    for (std::thread& thread : threads)
+    {
+        if (thread.joinable())
+            thread.join();
     }
 
     printf("Mandlebrot set generated\r\n");
 }
 
-void GenerateMandlebrotSetColoured(cv::Mat& image)
-{
-    const uint32_t maxIteration = 25;
-
-    uint i = 0, x = 0, y = 0;
-    float u = 0, v = 0;
-
-    uint32_t iteration = 0;
-    float fraction;
-    uint32_t value;
-
-    printf("Generating Mandlebrot Set Coloured with %i max iterations\r\n", maxIteration);
-
-    for (i = 0; i < size * size; i++)
+FractalFunction mandelbrotBW = [](uint32_t x, uint32_t y, uint32_t i, uint32_t mi, uchar value, cv::Mat* image)
     {
-        x = i % size;
-        y = floor(i / size);
+        image->at<cv::Vec3b>(y, x) = cv::Vec3b(value, value, value);
+    };
 
-        u = x / (float)size;
-        v = y / (float)size;
-
-        std::complex<float> z, c = { 2.0f * x / width - 1.5f, 2.0f * y / height - 1 };
-
-        for (iteration = 0; iteration < maxIteration; iteration++)
-        {
-            if (abs(z) >= 2)
-            {
-                break;
-            }
-            z = z * z + c;
-        }
-
-        fraction = iteration / (float)maxIteration;
-        value = fraction * 255;
-
-        // Inner of the set
-        if (iteration > maxIteration - 1)
-        {
-            image.at<cv::Vec3b>(y, x)[2] = 0;
-            image.at<cv::Vec3b>(y, x)[1] = 0;
-            image.at<cv::Vec3b>(y, x)[0] = 0;
-        }
-        else // Outer of the set
-        {
-            // Red
-            image.at<cv::Vec3b>(y, x)[2] = value * u;
-            // Green
-            image.at<cv::Vec3b>(y, x)[1] = value * v;
-            // Blue
-            image.at<cv::Vec3b>(y, x)[0] = value;
-        }
-    }
-
-    printf("Mandlebrot set coloured generated\r\n");
-}
-
-void GenerateMandlebrotSetHue(cv::Mat& image)
-{
-    const uint32_t maxIteration = 32;
-
-    uint i = 0, x = 0, y = 0;
-    float u = 0, v = 0;
-
-    uint32_t iteration = 0;
-    float fraction;
-    uint32_t value;
-
-    printf("Generating Mandlebrot Set Hue with %i max iterations\r\n", maxIteration);
-
-    for (i = 0; i < size * size; i++)
+FractalFunction mandelbrotColoured = [](uint32_t x, uint32_t y, uint32_t i, uint32_t mi, uchar value, cv::Mat* image)
     {
-        x = i % size;
-        y = floor(i / size);
+        // Red
+        image->at<cv::Vec3b>(y, x)[2] = value;
+        // Green
+        image->at<cv::Vec3b>(y, x)[1] = (x / (float)width) * 255;
+        // Blue
+        image->at<cv::Vec3b>(y, x)[0] = (y / (float)height) * 255;
+    };
 
-        u = x / (float)size;
-        v = y / (float)size;
-
-        std::complex<float> z, c = { 2.0f * x / width - 1.5f, 2.0f * y / height - 1 };
-
-        for (iteration = 0; iteration < maxIteration; iteration++)
+FractalFunction mandelbrotHue = [](uint32_t x, uint32_t y, uint32_t i, uint32_t mi, uchar value, cv::Mat* image)
+    {
+        if (i == mi)
         {
-            if (abs(z) >= 2)
-            {
-                break;
-            }
-            z = z * z + c;
+            image->at<cv::Vec3b>(y, x) = cv::Vec3b(0, 0, 0);
         }
-
-        fraction = iteration / (float)maxIteration;
-        value = fraction * 255;
-
-        // Inner of the set
-        if (iteration > maxIteration - 1)
-        {
-            image.at<cv::Vec3b>(y, x)[2] = 0;
-            image.at<cv::Vec3b>(y, x)[1] = 0;
-            image.at<cv::Vec3b>(y, x)[0] = 0;
-        }
-        else // Outer of the set
-        {
-            image.at<cv::Vec3b>(y, x)[0] = value * 0.705f; // hue (255 -> 180)
-            image.at<cv::Vec3b>(y, x)[1] = 255;            // saturation
-            image.at<cv::Vec3b>(y, x)[2] = 255;            // value
-        }
-    }
-    cv::cvtColor(image, image, cv::COLOR_HSV2BGR);
-
-    printf("Mandlebrot set hue generated\r\n");
-}
+        image->at<cv::Vec3b>(y, x) = cv::Vec3b(value * 0.705f, 255, 255);
+    };
 
 int main()
 {
     cv::Mat image(cv::Size(size, size), CV_8UC3, cv::Scalar(0, 0, 0));
 
-    cv::namedWindow("Image", cv::WINDOW_AUTOSIZE);
+    cv::namedWindow("Image", cv::WINDOW_NORMAL);
+
+    cv::resizeWindow("Image", cv::Size(768, 768));
 
     GenerateUV(image);
 
@@ -223,15 +174,16 @@ int main()
         }
         if (key == (int)'2')
         {
-            GenerateMandlebrotSet(image);
+            GenerateMandlebrotSet(&image, mandelbrotBW);
         }
         if (key == (int)'3')
         {
-            GenerateMandlebrotSetColoured(image);
+            GenerateMandlebrotSet(&image, mandelbrotColoured);
         }
         if (key == (int)'4')
         {
-            GenerateMandlebrotSetHue(image);
+            GenerateMandlebrotSet(&image, mandelbrotHue);
+            cvtColor(image, image, cv::COLOR_HSV2BGR);
         }
     }
 }
